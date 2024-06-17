23ac783d9081371897fe8fdcd8f3191902a1ae5b
padp721 <anggadp91@gmail.com>
2024-06-17 15:50:29 +0800

    Added fixture to pytest

--- unit-testing/tests/unit_testing/perhitungan_dasar/test_pengurangan.py
+++ unit-testing/tests/unit_testing/perhitungan_dasar/test_pengurangan.py
@@ -1,6 +1,6 @@
 from unit_testing.perhitungan_dasar.pengurangan import pengurangan
 
 
-def test_pengurangan():
-    assert pengurangan(10, 5) == 5
-    assert pengurangan(5, 10) == -5
+def test_pengurangan(constant: int):
+    assert pengurangan(10, constant) == 5
+    assert pengurangan(constant, 10) == -5
--- unit-testing/tests/unit_testing/perhitungan_dasar/test_penjumlahan.py
+++ unit-testing/tests/unit_testing/perhitungan_dasar/test_penjumlahan.py
@@ -1,5 +1,5 @@
 from unit_testing.perhitungan_dasar.penjumlahan import penjumlahan
 
 
-def test_penjumlahan():
-    assert penjumlahan(8, 5) == 13
+def test_penjumlahan(constant: int):
+    assert penjumlahan(8, constant) == 13
